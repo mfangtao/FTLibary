481738ca384f751f4c935e2d7dc41b3901ea5394
涛锅锅 <tao@tdb.vip>
2018-09-20 13:44:01 +0800

    添加Javatest

---
+++ ftlibrary/src/main/java/com/fangtao/ftlibrary/FFTest.java
@@ -0,0 +1,13 @@
+package com.fangtao.ftlibrary;
+
+/**
+ * 作者：涛锅锅
+ * 时间：下午1:43
+ * 邮箱：mfangtao@qq.com
+ */
+public class FFTest {
+    public static String test()
+    {
+        return "libary test";
+    }
+}
